b72f26c225d814c8eb70998425c3016d24768f1e
jose <jos3duardolopes@gmail.com>
2020-02-27 17:44:12 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Teste Dev Agrointeli
+
+### Start
+
+Run the startup application.
+
+instalando pacotes
+```js
+npm install  
+```
+
+startando aplicação
+```
+adonis serv --dev
+```
+ou
+```
+npm server
+```
---
+++ public/main.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  //elemento collapseble
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems);
+
+  //consumindo os dados
+  axios({
+    method: 'get',
+    url: 'http://www.mocky.io/v2/5e56eb9230000046d228eae4',
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    }
+  })
+  .then(response => {
+    montaTabela(response.data)
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+  //criando html
+  function montaTabela(data) {
+    console.log(data)
+    let area = 250;
+    let percentual = 0.3;
+    let lista = ''
+
+    data.forEach(function (praga) {
+      lista += "<li>"+
+        "<div class='collapsible-header'><i class='material-icons'>bug_report</i>"+praga.name +"</div>"+
+        "<div class='collapsible-body'><span>"+controle(praga.quantity, area)+"</span></div>"+
+        "</li>"
+    })
+    let listaHTML = document.getElementById('dados')
+    listaHTML.innerHTML = lista
+  }
+
+  //verificando propagação da praga
+  function controle(quantidade, area) {
+    let data = (quantidade / area) * 100
+    return data > 30 ? "<b>" + data.toFixed(2) + "%</b> - A incidência dessa praga está <b>fora</b> do controle." : "<b>" + data.toFixed(2) + "%</b> - A incidência dessa praga está <b>dentro</b> do controle."
+  }
+});
